5931dc0c1f6969217ab2b62510ed27d9b547ec5f
masatokondo <masatokondou0114@gmail.com>
2023-01-03 16:26:38 +0900

    9 commit

--- src/pages/05_mesure/01_fft/Fft.jsx
+++ src/pages/05_mesure/01_fft/Fft.jsx
@@ -162,7 +162,7 @@ const Fft = () => {
             let recDataTmp = {};
             // [{x:*, y:*, z:*}]の連想配列を作る
             recDataTmp.time = getTime[l]
-            recDataTmp.xAmp = getTime[l]
+            recDataTmp.xAmp = getX[l]
             recDataTmp.yAmp = getY[l]
             recDataTmp.zAmp = getZ[l]
             // 連想配列を配列に追加していく
